652479af52998dbeaf3e842ab1b524506cc018be
rajesh <peketirajesh007@gmail.com>
2023-08-12 21:12:55 +0530

    autoplay

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ var swiper = new Swiper(".mySwiper", {
     //slidesPerView: "3",
     loop: true,
     centeredSlides: true,
-    //autoplay: {
-    //  delay: 2500,
-    // disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
