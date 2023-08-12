8805fbe5f00fcd8f1b3794f02c078377e82c7eda
rajesh <peketirajesh007@gmail.com>
2023-08-12 19:13:52 +0530

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+const header = document.querySelector("#header")
+const topNav = document.querySelector(".topnav")
+const main = document.querySelector(".main")
+
+window.onscroll = () => {
+    if (window.scrollY >= 43) {
+        header.classList.add("active")
+    }
+    else {
+        header.classList.remove("active")
+    }
+}
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    slidesPerView: "3",
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
